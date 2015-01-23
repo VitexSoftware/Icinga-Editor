3feb0941eb8ff310cfd6eba4a04694e688c8a942
Vítězslav Dvořák <vitex@murka.cz>
2015-01-23 14:11:05 +0100

    Informace o tom které služby příkaz používají. Dokud je příkaz použit některou službou, je skryta nabídka smazání příkazu

--- command.php
+++ command.php
@@ -10,6 +10,7 @@
  */
 require_once 'includes/IEInit.php';
 require_once 'classes/IECommand.php';
+require_once 'classes/IEService.php';
 require_once 'classes/IECfgEditor.php';
 
 $oPage->onlyForLogged();
@@ -31,6 +32,13 @@ if ($delete == 'true') {
     $command->delete();
 }
 
+$service = new IEService;
+
+$usages = $service->getColumnsFromMySQL(array($service->getMyKeyColumn(), $service->nameColumn), array('check_command' => $command->getName()), $service->nameColumn, $service->getMyKeyColumn());
+
+
+
+
 $oPage->addItem(new IEPageTop(_('Editace příkazu') . ' ' . $command->getName()));
 
 $CommandEdit = new IECfgEditor($command);
@@ -41,16 +49,31 @@ if (!is_null($command->getMyKey())) {
     $form->addItem(new EaseHtmlInputHiddenTag($command->getmyKeyColumn(), $command->getMyKey()));
 }
 $form->addItem('<br>');
-$form->addItem(new EaseTWSubmitButton(_('Uložit'),'success'));
+$form->addItem(new EaseTWSubmitButton(_('Uložit'), 'success'));
 $oPage->AddCss('
 input.ui-button { width: 100%; }
 ');
 
-$oPage->columnIII->addItem($command->deleteButton());
 
 if ($command->getId()) {
     $oPage->columnI->addItem($command->ownerLinkButton());
 }
+
+if (count($usages)) {
+    $usedBy = new EaseTWBPanel(_('Používající služby'));
+    $listing = $usedBy->addItem(new EaseHtmlUlTag(null, array('class' => 'list-group')));
+    foreach ($usages as $usage) {
+        $listing->addItem(
+            new EaseHtmlLiTag(
+            new EaseHtmlATag('service.php?service_id=' . $usage['service_id'], $usage[$service->nameColumn])
+            , array('class' => 'list-group-item'))
+        );
+    }
+    $form = $oPage->columnI->addItem($usedBy);
+} else {
+    $oPage->columnIII->addItem($command->deleteButton());
+}
+
 
 $oPage->addItem(new IEPageBottom());
 
