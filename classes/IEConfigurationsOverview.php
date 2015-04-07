14d073421086b9789af7aa622275b24818917748
Vítězslav Dvořák <info@vitexsoftware.cz>
2015-04-07 12:04:09 +0200

    Přehled hostů bez aktuální konfigurace

---
+++ classes/IEConfigurationsOverview.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of IEConfigurationsOvervier
+ *
+ * @author vitex
+ */
+class IEConfigurationsOverview extends EaseTWBPanel
+{
+
+    public function __construct($hosts)
+    {
+        $ok = 0;
+        $hosts_table = new EaseHtmlTableTag(null, array('class' => 'table'));
+        foreach ($hosts as $host_id => $host_info) {
+            if (is_null($host_info['host_name']) || !strlen($host_info['host_name'])) {
+                unset($hosts[$host_id]);
+                continue;
+            }
+
+            if (isset($host_info['config_hash'])) {
+                $host = new IEHost((int) $host_id);
+                if ($host->getConfigHash() == $host_info['config_hash']) {
+                    unset($hosts[$host_id]);
+                    $ok++;
+                    continue;
+                } else {
+                    //Zastaralá konfigurace
+                    $hosts_table->addRowColumns(array(new EaseHtmlATag('host.php?host_id=' . $host_id, $host_info['host_name']), new EaseTWBLinkButton('sensor.php?host_id=' . $host_id, _('aktualizovat senzor'))), array('class' => 'warning'));
+                }
+            } else {
+                //senzor neregistrován
+                $hosts_table->addRowColumns(array(new EaseHtmlATag('host.php?host_id=' . $host_id, $host_info['host_name']), new EaseTWBLinkButton('sensor.php?host_id=' . $host_id, _('nasadit senzor'))), array('class' => 'warning'));
+            }
+        }
+        parent::__construct(_('Hosty s neaktuální konfigurací'), 'info', $hosts_table, sprintf(_('Celkem %s hostů bez aktuální konfigurace. (%s aktuální)'), count($hosts), $ok));
+    }
+
+}
--- main.php
+++ main.php
@@ -9,24 +9,18 @@
  * @copyright  2012 Vitex@hippy.cz (G)
  */
 require_once 'includes/IEInit.php';
-require_once 'classes/IEContact.php';
-require_once 'classes/IEContactgroup.php';
-require_once 'classes/IEHost.php';
-require_once 'classes/IEHostgroup.php';
-require_once 'classes/IETimeperiod.php';
-require_once 'classes/IECommand.php';
-require_once 'classes/IEServicegroup.php';
 
 $oPage->onlyForLogged();
 
 $oPage->addItem(new IEPageTop(_('Monitoring')));
-$oPage->addPageColumns();
 
 $host = new IEHost();
-$hosts = $host->getListing(null, false);
+$hosts = $host->getListing(null, false, array('config_hash', $host->myCreateColumn, $host->myLastModifiedColumn));
 
-if (!count($hosts)) {
-    $oPage->columnII->addItem(new EaseTWBLinkButton('wizard.php', _('Založte si první sledovaný host'), 'success'));
+if (count($hosts)) {
+    $oPage->container->addItem(new IEConfigurationsOverview($hosts));
+} else {
+    $oPage->container->addItem(new EaseTWBLinkButton('wizard.php', _('Založte si první sledovaný host'), 'success'));
     $oUser->addStatusMessage(_('Zatím není zaregistrovaný žádný sledovaný host'), 'warning');
 }
 
