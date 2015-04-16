a6a369f1b29c0aa3363c74eb73f0da43a48ea91f
Vítězslav Dvořák <info@vitexsoftware.cz>
2015-04-16 16:35:52 +0200

    Konfigurační přehled rozšířen o hosty bez rodičů, ikony a kontaktů.

--- classes/IEConfigurationsOverview.php
+++ classes/IEConfigurationsOverview.php
@@ -22,6 +22,9 @@ class IEConfigurationsOverview extends EaseTWBPanel
 
         $noSensor = array();
         $oldSensor = array();
+        $noParents = array();
+        $noIcon = array();
+        $noContacts = array();
 
         foreach ($hosts as $host_id => $host_info) {
             if (is_null($host_info['host_name']) || !strlen($host_info['host_name'])) {
@@ -43,6 +46,21 @@ class IEConfigurationsOverview extends EaseTWBPanel
                 $noSensor[$host_id] = $host_info;
                 //senzor neregistrován
             }
+
+            if (!isset($host_info['parents']) || !count($host_info['parents'])) {
+                $noParents[$host_id] = $host_info;
+                //Host bez rodičů
+            }
+
+            if (!isset($host_info['icon_image']) || !strlen(trim($host_info['icon_image']))) {
+                $noIcon[$host_id] = $host_info;
+                //Host bez ikony
+            }
+
+            if ((!isset($host_info['contacts']) || !count($host_info['contacts'])) || (!isset($host_info['contact_groups']) || !count($host_info['contact_groups']))) {
+                $noContacts[$host_id] = $host_info;
+                //Host bez kontaktů
+            }
         }
 
         $hostsTabs = new EaseTWBTabs('hostsTabs');
@@ -52,16 +70,43 @@ class IEConfigurationsOverview extends EaseTWBPanel
                 $row = $oldHostsTable->addRowColumns(array(new EaseHtmlATag('host.php?host_id=' . $host_id, $host_info['host_name']), new EaseTWBLinkButton('sensor.php?host_id=' . $host_id, _('aktualizovat senzor'))));
                 $row->setTagClass('warning');
             }
-            $hostsTabs->addTab(sprintf(_('Neaktuální <span class="badge">%s</span>'), count($oldSensor)), $oldHostsTable);
+            $hostsTabs->addTab(sprintf(_('Neakt. Senzor <span class="badge">%s</span>'), count($oldSensor)), $oldHostsTable);
         }
 
         if (count($noSensor)) {
-            $noHostsTable = new EaseHtmlTableTag(null, array('class' => 'table'));
+            $noSensorTable = new EaseHtmlTableTag(null, array('class' => 'table'));
             foreach ($hosts as $host_id => $host_info) {
-                $row = $noHostsTable->addRowColumns(array(new EaseHtmlATag('host.php?host_id=' . $host_id, $host_info['host_name']), new EaseTWBLinkButton('sensor.php?host_id=' . $host_id, _('nasadit senzor'))));
+                $row = $noSensorTable->addRowColumns(array(new EaseHtmlATag('host.php?host_id=' . $host_id, $host_info['host_name']), new EaseTWBLinkButton('sensor.php?host_id=' . $host_id, _('nasadit senzor'))));
                 $row->setTagClass('danger');
             }
-            $hostsTabs->addTab(sprintf(_('Bez senzoru <span class="badge">%s</span>'), count($noSensor)), $noHostsTable);
+            $hostsTabs->addTab(sprintf(_('Bez senzoru <span class="badge">%s</span>'), count($noSensor)), $noSensorTable);
+        }
+
+        if (count($noParents)) {
+            $noParentsTable = new EaseHtmlTableTag(null, array('class' => 'table'));
+            foreach ($hosts as $host_id => $host_info) {
+                $row = $noParentsTable->addRowColumns(array(new EaseHtmlATag('host.php?host_id=' . $host_id, $host_info['host_name']), new EaseTWBLinkButton('host.php?action=parent&host_id=' . $host_id, _('přiřadit rodiče'))));
+                $row->setTagClass('info');
+            }
+            $hostsTabs->addTab(sprintf(_('Bez rodičů <span class="badge">%s</span>'), count($noParents)), $noParentsTable);
+        }
+
+        if (count($noIcon)) {
+            $noIconTable = new EaseHtmlTableTag(null, array('class' => 'table'));
+            foreach ($hosts as $host_id => $host_info) {
+                $row = $noIconTable->addRowColumns(array(new EaseHtmlATag('host.php?host_id=' . $host_id, $host_info['host_name']), new EaseTWBLinkButton('host.php?action=icon&host_id=' . $host_id, _('přiřadit ikonu'))));
+                $row->setTagClass('default');
+            }
+            $hostsTabs->addTab(sprintf(_('Bez ikony <span class="badge">%s</span>'), count($noIcon)), $noIconTable);
+        }
+
+        if (count($noContacts)) {
+            $noContactsTable = new EaseHtmlTableTag(null, array('class' => 'table'));
+            foreach ($hosts as $host_id => $host_info) {
+                $row = $noContactsTable->addRowColumns(array(new EaseHtmlATag('host.php?host_id=' . $host_id, $host_info['host_name']), new EaseTWBLinkButton('host.php?host_id=' . $host_id, _('přiřadit kontakty'))));
+                $row->setTagClass('default');
+            }
+            $hostsTabs->addTab(sprintf(_('Bez kontaktů <span class="badge">%s</span>'), count($noIcon)), $noContactsTable);
         }
 
         parent::__construct(_('Hosty dle stavu konfigurace'), 'info', $hostsTabs, sprintf(_('Celkem %s hostů bez aktuální konfigurace. (%s aktuální)'), count($hosts), $ok));
--- classes/IEWebPage.php
+++ classes/IEWebPage.php
@@ -1,6 +1,6 @@
 <?php
 
-define('IE_VERSION', '0.138');
+define('IE_VERSION', '0.139');
 
 /**
  * Třídy pro vykreslení stránky
