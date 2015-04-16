e2ce16559ede6d9ae53a62daf3d8873020293adf
Vítězslav Dvořák <info@vitexsoftware.cz>
2015-04-16 12:40:38 +0200

    Chyba #107 - černá průhlednost opravena na bílou

--- classes/IEIconSelector.php
+++ classes/IEIconSelector.php
@@ -204,6 +204,33 @@ class IEIconSelector extends EaseContainer
         }
 
         $thumbnailGdImage = imagecreatetruecolor($thumbnailImageWidth, $thumbnailImageHeight);
+
+        switch ($sourceImageType) {
+            case IMAGETYPE_PNG:
+                // integer representation of the color black (rgb: 0,0,0)
+                $background = imagecolorallocate($sourceGdImage, 0, 0, 0);
+                // removing the black from the placeholder
+                imagecolortransparent($thumbnailGdImage, $background);
+
+                // turning off alpha blending (to ensure alpha channel information
+                // is preserved, rather than removed (blending with the rest of the
+                // image in the form of black))
+                imagealphablending($thumbnailGdImage, false);
+
+                // turning on alpha channel information saving (to ensure the full range
+                // of transparency is preserved)
+                imagesavealpha($sourceGdImage, true);
+
+                break;
+            case IMAGETYPE_GIF:
+                // integer representation of the color black (rgb: 0,0,0)
+                $background = imagecolorallocate($sourceGdImage, 0, 0, 0);
+                // removing the black from the placeholder
+                imagecolortransparent($thumbnailGdImage, $background);
+
+                break;
+        }
+
         imagecopyresampled($thumbnailGdImage, $sourceGdImage, 0, 0, 0, 0, $thumbnailImageWidth, $thumbnailImageHeight, $sourceImageWidth, $sourceImageHeight);
 
         switch ($sourceImageType) {
