9cc56298eb295df503846728326f10f0e20dbc4d
Vítězslav Dvořák <info@vitexsoftware.cz>
2015-04-24 19:37:29 +0200

    Nástroj pro rychlé vyplnění cesty k hostu.

--- cfgconfirm.php
+++ cfgconfirm.php
@@ -18,10 +18,23 @@ if ($host_id && $hash) {
     $host->setMyKey($host_id);
     $host->setDataValue('config_hash', $hash);
     if ($host->updateToMySQL()) {
-        echo _('Konfigurace potvrzena');
+        echo sprintf(_('Konfigurace %s potvrzena'), $host->getName());
     } else {
-        echo _('Chyba potvrzení konfigurace');
+        echo sprintf(_('Chyba potvrzení konfigurace'), $host->getName());
     }
+    echo "\n<br>" . _('Sledovat cestu k hostu') . ":\n";
+
+    if (isset($_SERVER['REQUEST_SCHEME'])) {
+        $scheme = $_SERVER['REQUEST_SCHEME'];
+    } else {
+        $scheme = 'http';
+    }
+
+    $enterPoint = $scheme . '://' . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI']) . '/';
+    $enterPoint = str_replace('\\', '', $enterPoint); //Win Hack
+    $confirmUrl = $enterPoint . 'watchroute.php?action=parent&host_id=' . $host_id . '&ip=' . $_SERVER['REMOTE_ADDR'];
+
+    echo '<a href="' . $confirmUrl . '">' . $confirmUrl . '</a>';
 } else {
     die(_('Chybné volání'));
 }
--- classes/IEWebPage.php
+++ classes/IEWebPage.php
@@ -1,6 +1,6 @@
 <?php
 
-define('IE_VERSION', '0.147');
+define('IE_VERSION', '0.148');
 
 /**
  * Třídy pro vykreslení stránky
